57f5079ede47a22fbf148b927173b7ffa4d1a775
Kaito Sugimoto <hellorusk1998@gmail.com>
2025-12-16 22:11:43 +0900

    Add new recipes: きのことほうれん草のミルクスープ野菜, 豚とキャベツの味噌煮込みうどん

--- data.js
+++ data.js
@@ -14,5 +14,15 @@ const cookingData = [
     "name": "鶏肉と野菜の具だくさんおかずスープ",
     "url": "https://park.ajinomoto.co.jp/recipe/card/801743/",
     "ingredients": ["鶏もも肉", "キャベツ", "ほうれん草", "しめじ"]
+  },
+  {
+    "name": "きのことほうれん草のミルクスープ野菜",
+    "url": "https://park.ajinomoto.co.jp/recipe/card/710464/",
+    "ingredients": ["しめじ", "ほうれん草", "ベーコン", "牛乳"]
+  },
+  {
+    "name": "豚とキャベツの味噌煮込みうどん",
+    "url": "https://www.kurashiru.com/recipes/2d84b7cb-ce30-4c0d-bd33-b1934f50a914",
+    "ingredients": ["うどん", "豚ロース", "キャベツ", "長ねぎ", "にんにく"]
   }
 ];
